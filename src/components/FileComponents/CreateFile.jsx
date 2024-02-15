691cceb57ce0e87a270aabd2312c6aedc1cc37c9
kapalfa <androkosm2000@gmail.com>
2024-02-15 16:10:22 +0200

    CreateFile correction

--- src/components/FileComponents/CreateFile.jsx
+++ src/components/FileComponents/CreateFile.jsx
@@ -7,7 +7,7 @@ import Button from '@mui/material/Button'
 const createFile = ({filename, path, axiosPrivate}) => {
     console.log("path ", path)
     console.log("filename: ", filename)
-    axiosPrivate.post(`/createFile/${path}`, filename,
+    axiosPrivate.post(`/createFile/${path}`, {filename: filename},
     {
         headers: {
             'Content-Type': 'application/json'
