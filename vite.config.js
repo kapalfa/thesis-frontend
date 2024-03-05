c8f140564bc541e7889d8579519cd054acc8b751
kapalfa <androkosm2000@gmail.com>
2024-03-05 12:13:00 +0200

    correct target at vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/": {
-        target: "https://code-editor-eusldaqlhq-zf.a.run.app",
+        target: "https://thesis-backend-eusldaqlhq-uc.a.run.app",
         changeOrigin: true,
       },
     },
